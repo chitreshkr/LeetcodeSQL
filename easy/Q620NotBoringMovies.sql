318f27fed561d383142f5c06088dc236331c0bcb
Chitresh Kumar <8807772+chitreshkr@users.noreply.github.com>
2021-03-19 13:30:37 -0500

    Add files via upload

---
+++ easy/Q620NotBoringMovies.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+#We will select the columns and use the where clause to filter odd ids and description is not boring
+#Then we will order the rating column in descending order
+
+Select id,movie , description, rating
+from cinema
+where mod(id,2) <> 0 AND description != 'Boring' 
+group by id
+order by rating DESC;
