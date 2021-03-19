7bca060f2eee488b1299020307aa172516477a7f
Chitresh Kumar <8807772+chitreshkr@users.noreply.github.com>
2021-03-19 13:31:41 -0500

    Update Q620NotBoringMovies.sql

--- easy/Q620NotBoringMovies.sql
+++ easy/Q620NotBoringMovies.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-
+# https://leetcode.com/problems/not-boring-movies/
 #We will select the columns and use the where clause to filter odd ids and description which is not boring
 #Then we will order the rating column in descending order
 
